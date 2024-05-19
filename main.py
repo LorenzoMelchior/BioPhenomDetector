ca13db0c86f29193e92de4121d4c80c5e8824760
Lorenzo Melchior <lorenzo_melchior@me.com>
2024-05-19 15:46:09 +0200

    added first stub

--- main.py
+++ main.py
@@ -1,7 +1,16 @@
+import numpy as np
+
+
+def compute_standardized_anomalies(raw_data: np.ndarray,
+                                   daily_averages: np.ndarray,
+                                   locations: np.ndarray) -> np.ndarray:
+    pass
+
 
 def main() -> None:
-    pass
+    compute_standardized_anomalies(None, None, None)
 
 
 if __name__ == '__main__':
     main()
+
