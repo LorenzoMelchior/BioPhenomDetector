a158b9f86affb15fc61a6845d84ce6c638f11651
Lorenzo Melchior <lorenzo_melchior@me.com>
2024-08-10 21:57:12 +0200

    linted main

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 import numpy as np
+import pandas as pd
 
 
-def compute_standardized_anomalies(raw_data: np.ndarray,
-                                   daily_averages: np.ndarray,
-                                   locations: np.ndarray) -> np.ndarray:
+def compute_standardized_anomalies(
+    raw_data: np.ndarray, daily_averages: np.ndarray, locations: np.ndarray
+) -> np.ndarray:
     pass
 
 
@@ -11,6 +12,5 @@ def main() -> None:
     compute_standardized_anomalies(None, None, None)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
-
